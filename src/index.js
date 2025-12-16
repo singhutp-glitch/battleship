d7993a36733afdc9b33cbef32d9b9d157c50bca4
singhutp-glitch <singhutp@gmail.com>
2025-12-16 12:04:55 +0530

    Make new game and basic html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import "./styles.css";
 console.log("hello");
+const Player=require('./player');
+
+class NewGame{
+    
+    constructor()
+    {
+        this.player1=new Player(1);
+        this.player2=new Player(0);
+        this.setup(this.player1);
+        this.setup(this.player2);
+    }
+    setup(player)
+    {
+        player.gameBoard.placeShip([1,1],3,1);
+        player.gameBoard.placeShip([2,1],2,1);
+        player.gameBoard.placeShip([3,1],1,1);
+    }
+    
+}
